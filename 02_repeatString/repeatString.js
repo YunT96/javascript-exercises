1b51760eafd141424ab55d206e43b04d8f35877a
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-05-24 21:44:04 +0800

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(greeting, times) {
+    let fullMessage = "";
 
+    if (times < 0){
+        console.log("ERROR");
+        return("ERROR");
+    }
+    else{
+        for (let i = 0; i < times; i++){
+            fullMessage += greeting;
+        }
+        console.log(fullMessage);
+        return fullMessage;
+    }
+    
 };
 
 // Do not edit below this line
