f7a820bed0ba6f1451e2c4883e82da62098617b8
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-05-27 16:06:28 +0800

    Complete Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
 
+    if (typeof intOne != "number" || typeof intTwo != "number" || intOne < 0 || intTwo < 0){
+        return('ERROR')
+    }
+    
+    if (intOne > intTwo) {
+        // Swap intOne and intTwo
+        let temp = intOne;
+        intOne = intTwo;
+        intTwo = temp;
+    }
+
+    let sum = 0;
+    for (let i = intOne; i <= intTwo; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
