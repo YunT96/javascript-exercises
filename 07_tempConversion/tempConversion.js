4d1bd9962d92aa94bc51cf3758a7786dc55239b2
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-05-27 16:41:51 +0800

    Complete Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celsius = (num - 32) * 5/9;
+  celsius = parseFloat(celsius.toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let fahrenheit = (num  * 9/5) + 32;
+  fahrenheit = parseFloat(fahrenheit.toFixed(1));
+  return fahrenheit;
 };
 
 // Do not edit below this line
