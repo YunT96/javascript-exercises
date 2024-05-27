e969b2e65da4dcb68dd9bbab2f8c4f518c12d470
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-05-27 15:34:09 +0800

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...num) {
+   return array.filter(item => !num.includes(item));
 };
+//Using arrow function expression =>
+//the item in this code works like [i] in a traditional loop
 
 // Do not edit below this line
 module.exports = removeFromArray;
